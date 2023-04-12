d9bd38cd58914896dc6177451b97b81f8dca64a7
Alex Liberzon <alexlib@users.noreply.github.com>
2023-04-12 20:29:36 +0300

    initial copy of liboptv and pybind tests

--- tests/test_rotation_matrix.py
+++ tests/test_rotation_matrix.py
@@ -1,11 +1,16 @@
 import numpy as np
 import pytest
-from openptv_python.calibration import rotation_matrix, Exterior
+
+from openptv_python.calibration import Exterior, rotation_matrix
+
 
 # Define some test fixtures
 @pytest.fixture
 def exterior():
-    return Exterior(phi=0, omega=0, kappa=0, dm=np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
+    return Exterior(
+        phi=0, omega=0, kappa=0, dm=np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    )
+
 
 # Define some test cases
 def test_rotation_matrix_0(exterior):
@@ -13,14 +18,16 @@ def test_rotation_matrix_0(exterior):
     result = rotation_matrix(exterior)
     assert np.all(result.dm == [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
+
 def test_rotation_matrix_pi_2(exterior):
     # Test when all angles are pi/2
-    exterior.phi = np.pi/2
-    exterior.omega = np.pi/2
-    exterior.kappa = np.pi/2
+    exterior.phi = np.pi / 2
+    exterior.omega = np.pi / 2
+    exterior.kappa = np.pi / 2
     result = rotation_matrix(exterior)
     print(result.dm)
-    assert np.allclose(result.dm ,np.array([[0, 0, 1], [0, -1, 0], [1, 0, 0]]))
+    assert np.allclose(result.dm, np.array([[0, 0, 1], [0, -1, 0], [1, 0, 0]]))
+
 
 def test_rotation_matrix_pi(exterior):
     # Test when all angles are pi
@@ -30,15 +37,21 @@ def test_rotation_matrix_pi(exterior):
     result = rotation_matrix(exterior)
     print(result.dm)
     assert np.allclose(result.dm, np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
-    
+
+
 def test_opposite_rotation():
-    ex = Exterior(phi=np.pi, omega=np.pi, kappa=np.pi, dm=[[-1, 0, 0], [0, -1, 0], [0, 0, 1]])
+    ex = Exterior(
+        phi=np.pi, omega=np.pi, kappa=np.pi, dm=[[-1, 0, 0], [0, -1, 0], [0, 0, 1]]
+    )
     # Rotate the matrix using the rotation_matrix function
     ex_rotated = rotation_matrix(ex)
-    assert(np.allclose(ex_rotated.dm, np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])))
-    
+    assert np.allclose(ex_rotated.dm, np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
+
+
 def test_manual_rotation_pi():
-    Ex = Exterior(phi=np.pi, omega=np.pi, kappa=np.pi, dm=[[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    Ex = Exterior(
+        phi=np.pi, omega=np.pi, kappa=np.pi, dm=[[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+    )
 
     cp = np.cos(Ex.phi)
     sp = np.sin(Ex.phi)
@@ -59,6 +72,5 @@ def test_manual_rotation_pi():
     Ex.dm[2][2] = co * cp
 
     Ex.dm = np.round(Ex.dm, 7)
-    
-    assert(np.allclose(Ex.dm, np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])))
-    
+
+    assert np.allclose(Ex.dm, np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
