fixed also rotation matrix test

openptv_python/lsqadj.py
@@ -76,44 +76,44 @@ def atl(u: np.ndarray, a: np.ndarray, b: np.ndarray, m: int, n: int, n_large: in
 # print(A_inv)
 
 
-def matinv(a: np.ndarray, n: int, n_large: int) -> np.ndarray:
-    """
-    Calculate inverse of a matrix A, with the option of working with the sub-vector only, when n < n_large.
+# def matinv(a: np.ndarray, n: int, n_large: int) -> np.ndarray:
+#     """
+#     Calculate inverse of a matrix A, with the option of working with the sub-vector only, when n < n_large.
 
-    Arguments:
-    ---------
-    a - matrix of doubles of the size (n_large x n_large).
-    n - size of the output - size of the sub-matrix, number of observations
-    n_large - number of rows and columns in matrix a
-    """
-    if a.shape != (n_large, n_large):
-        raise ValueError("a has wrong shape")
+#     Arguments:
+#     ---------
+#     a - matrix of doubles of the size (n_large x n_large).
+#     n - size of the output - size of the sub-matrix, number of observations
+#     n_large - number of rows and columns in matrix a
+#     """
+#     if a.shape != (n_large, n_large):
+#         raise ValueError("a has wrong shape")
 
-    # a = a.flatten(order="C")
+#     # a = a.flatten(order="C")
 
-    for ipiv in range(n):
-        pivot = a[ipiv * n_large + ipiv]
-        print(f"pivot = {pivot}")
-        if np.all(pivot == 0.0):
-            raise ValueError("pivot is zero")
+#     for ipiv in range(n):
+#         pivot = a[ipiv * n_large + ipiv]
+#         print(f"pivot = {pivot}")
+#         if np.all(pivot == 0.0):
+#             raise ValueError("pivot is zero")
 
-        pivot = 1.0 / pivot
-        npivot = -pivot
-        for irow in range(n):
-            for icol in range(n):
-                if irow != ipiv and icol != ipiv:
-                    a[irow * n_large + icol] -= (
-                        a[ipiv * n_large + icol] * a[irow * n_large + ipiv] * pivot
-                    )
-        for icol in range(n):
-            if ipiv != icol:
-                a[ipiv * n_large + icol] *= npivot
-        for irow in range(n):
-            if ipiv != irow:
-                a[irow * n_large + ipiv] *= pivot
-        a[ipiv * n_large + ipiv] = pivot
+#         pivot = 1.0 / pivot
+#         npivot = -pivot
+#         for irow in range(n):
+#             for icol in range(n):
+#                 if irow != ipiv and icol != ipiv:
+#                     a[irow * n_large + icol] -= (
+#                         a[ipiv * n_large + icol] * a[irow * n_large + ipiv] * pivot
+#                     )
+#         for icol in range(n):
+#             if ipiv != icol:
+#                 a[ipiv * n_large + icol] *= npivot
+#         for irow in range(n):
+#             if ipiv != irow:
+#                 a[irow * n_large + ipiv] *= pivot
+#         a[ipiv * n_large + ipiv] = pivot
 
-    return a.reshape(n, n)
+#     return a.reshape(n, n)
 
 
 # def matmul(b: np.ndarray, c: np.ndarray, m: int, n: int, k: int):

tests/test_lsqadj.py
@@ -4,7 +4,7 @@ import unittest
 import numpy as np
 
 from openptv_python.calibration import Exterior
-from openptv_python.lsqadj import ata, atl, matinv, matmul
+from openptv_python.lsqadj import ata, atl, matmul
 
 EPS = 1e-6
 
@@ -103,30 +103,6 @@ class TestATL(unittest.TestCase):
         for i in range(3):
             msg = f"wrong item [{i}] {u[i]} instead of {expected[i]}"
             self.assertTrue(math.isclose(u[i], expected[i], rel_tol=EPS), msg)
-
-
-class TestMatinv(unittest.TestCase):
-    """test the inverse of a 3x3 matrix."""
-
-    def test_matinv(self):
-        """Test the inverse of a 3x3 matrix."""
-        c = np.array([[1, 2, 3], [0, 4, 5], [1, 0, 6]], float)
-        expected = np.array(
-            [
-                [1.090909, -0.545455, -0.090909],
-                [0.227273, 0.136364, -0.227273],
-                [-0.181818, 0.090909, 0.181818],
-            ]
-        )
-
-        print(c)
-        print(np.linalg.inv(c))
-        c = matinv(c, 3, 3)
-        print(c - expected)
-
-        for i in range(3):
-            for j in range(3):
-                self.assertTrue(math.isclose(c[i][j], expected[i][j], abs_tol=EPS))
 
 
 if __name__ == "__main__":

tests/test_rotation_matrix.py
@@ -1,22 +1,31 @@
 import numpy as np
 import pytest
 
-from openptv_python.calibration import Exterior, rotation_matrix
+from openptv_python.calibration import Exterior
 
 
 # Define some test fixtures
 @pytest.fixture
 def exterior():
+    """Create an exterior object with all angles set to zero."""
     return Exterior(
-        phi=0, omega=0, kappa=0, dm=np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        x0=0,
+        y0=0,
+        z0=0,
+        phi=0,
+        omega=0,
+        kappa=0,
+        dm=np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]),
     )
 
 
 # Define some test cases
 def test_rotation_matrix_0(exterior):
+    """Test when all angles are zero."""
     # Test when all angles are zero
-    result = rotation_matrix(exterior)
-    assert np.all(result.dm == [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    exterior.update_rotation_matrix()
+    # rotation_matrix(exterior)
+    assert np.all(exterior.dm == [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
 
 def test_rotation_matrix_pi_2(exterior):
@@ -24,9 +33,9 @@ def test_rotation_matrix_pi_2(exterior):
     exterior.phi = np.pi / 2
     exterior.omega = np.pi / 2
     exterior.kappa = np.pi / 2
-    result = rotation_matrix(exterior)
-    print(result.dm)
-    assert np.allclose(result.dm, np.array([[0, 0, 1], [0, -1, 0], [1, 0, 0]]))
+    exterior.update_rotation_matrix()
+    # rotation_matrix(exterior)
+    assert np.allclose(exterior.dm, np.array([[0, 0, 1], [0, -1, 0], [1, 0, 0]]))
 
 
 def test_rotation_matrix_pi(exterior):
@@ -34,9 +43,8 @@ def test_rotation_matrix_pi(exterior):
     exterior.phi = np.pi
     exterior.omega = np.pi
     exterior.kappa = np.pi
-    result = rotation_matrix(exterior)
-    print(result.dm)
-    assert np.allclose(result.dm, np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
+    exterior.update_rotation_matrix()
+    assert np.allclose(exterior.dm, np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
 
 
 def test_opposite_rotation():
@@ -44,8 +52,8 @@ def test_opposite_rotation():
         phi=np.pi, omega=np.pi, kappa=np.pi, dm=[[-1, 0, 0], [0, -1, 0], [0, 0, 1]]
     )
     # Rotate the matrix using the rotation_matrix function
-    ex_rotated = rotation_matrix(ex)
-    assert np.allclose(ex_rotated.dm, np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
+    ex.update_rotation_matrix()
+    assert np.allclose(ex.dm, np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
 
 
 def test_manual_rotation_pi():